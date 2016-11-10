2cc109ece4265e28b643338a2cfeb58307e7fcb1
Guilherme Santos <sntguilherme@gmail.com>
2016-11-10 19:19:34 -0200

    handlebards pt. 1

---
+++ aulas/aula-8/atividade-1/js/app.js
@@ -0,0 +1,143 @@
+'use strict';
+
+$(function () {
+    configureSearchButton();
+    configureSearchInput();
+    configureClearSearchButton();
+
+    function configureSearchButton() {
+        $('.user-github-search .btn-search').on('click', function () {
+            searchRemoteData();
+        });
+    }
+
+    function configureSearchInput() {
+        $('.user-github-search__form input:text').on('keypress', function (event) {
+            if (event.keyCode !== 13) {
+                return;
+            }
+            event.preventDefault();
+            searchRemoteData();
+        });
+    }
+
+    function getSearchText() {
+        return $('.user-github-search__form input:text').val();
+    }
+
+    function searchRemoteData() {
+        var userName = getSearchText();
+
+        if (!userName) {
+            $('.user-github-details').hide('slow');
+            $('.search-not-found').hide('slow');
+            $('.user-github-search .clear-search a').hide();
+            return;
+        }
+
+        $.loadingOverlay.start();
+
+        var templatesPromise = getTemplateRepositoryPromise();
+        var userPromise = findGithubUser(userName);
+        var userReposPromise = findGithubRepos(userName);
+
+        $.when(templatesPromise, userPromise, userReposPromise)
+            .done(function (templates, user, userRepos) {
+                configureViewWithSuccessResponse(templates[0], user[0], userRepos[0]);
+            })
+            .fail(function (jqXHR, textStatus, exception) {
+                jqXHR.status === 404 && userName && configureViewWithErrorResponse(userName);
+            })
+            .always(function () {
+                $.loadingOverlay.stop();
+            });
+    }
+
+    function getTemplateRepositoryPromise() {
+        return $.ajax({url: 'templates/repo-list-item.html', cache: false});
+    }
+
+    function findGithubUser(userName) {
+        return $.ajax({
+            url: "https://api.github.com/users/" + userName,
+            type: 'GET',
+            async: true,
+            dataType: 'json'
+        });
+    }
+
+    function findGithubRepos(userName) {
+        return $.ajax({
+            url: "https://api.github.com/users/" + userName + '/repos',
+            type: 'GET',
+            async: true,
+            dataType: 'json'
+        });
+    }
+
+    function configureViewWithSuccessResponse(templates, user, userRepos) {
+        configurePersonalInfo(user.avatar_url, user.name, user.email, user.blog, user.company);
+        configurePersonalBio(user.bio);
+        configureStatistics(user.public_repos, user.followers, user.following);
+        configureRepositories(templates, userRepos);
+
+        $('.search-not-found').hide('slow');
+        $('.user-github-details').show('slow');
+        $('.user-github-search .clear-search a').show();
+    }
+
+    function configurePersonalInfo(imageUrl, name, email, site, company) {
+        $('.user-github-details .user-github-details__personal .image').css('background-image', 'url("' + imageUrl + '")');
+
+        $('.user-github-details .user-github-details__personal .info .info__name .value').text(name);
+
+        $('.user-github-details .user-github-details__personal .info .info__email .value').text(email);
+        $('.user-github-details .user-github-details__personal .info .info__email')[email ? 'show' : 'hide']();
+
+        $('.user-github-details .user-github-details__personal .info .info__site .value').text(site);
+        $('.user-github-details .user-github-details__personal .info .info__site')[site ? 'show' : 'hide']();
+
+        $('.user-github-details .user-github-details__personal .info .info__company .value').text(company);
+        $('.user-github-details .user-github-details__personal .info .info__company')[company ? 'show' : 'hide']();
+
+    }
+
+    function configureStatistics(repos, followers, followings) {
+        $('.user-github-details .user-github-details__statistics .user-repos .value').text(repos);
+        $('.user-github-details .user-github-details__statistics .user-followers .value').text(followers);
+        $('.user-github-details .user-github-details__statistics .user-following .value').text(followings);
+    }
+
+    function configurePersonalBio(bio) {
+        !bio && $('.user-github-details__bio').hide();
+        bio && $('.user-github-details__bio p').text(bio).show();
+    }
+
+    function configureRepositories(template, repositories) {
+        var $template = Handlebars.compile(template);
+
+        $('.user-github-details__repos .repos-list').empty();
+
+        repositories.forEach(function (repository) {
+            $($template(repository)).appendTo('.user-github-details__repos .repos-list');
+        });
+
+        $('.user-github-details__repos')[repositories.length ? 'show' : 'hide']();
+    }
+
+    function configureViewWithErrorResponse(userName) {
+        $('.search-not-found').html('Nenhum resultado encontrado com o usuário "<em>' + userName + '</em>".').show('slow');
+        $('.user-github-details').hide('slow');
+    }
+
+    function configureClearSearchButton() {
+        $('.user-github-search .clear-search a').hide().confirmButton('Deseja mesmo limpar a pesquisa?', function () {
+            $('.user-github-search__form input:text').val('').focus();
+
+            $('.user-github-details').hide('slow');
+            $('.search-not-found').hide('slow');
+            $('.user-github-search .clear-search a').hide();
+        });
+    }
+})
+;
